29efe0e149e6466e4af85cab91c1e2e41f35eb65
karbasappa018 <karbasappatadakale@gmail.com>
2026-01-31 22:43:51 +0530

    Add example demonstrating if-else condition to find maximum number

---
+++ program15.js
@@ -0,0 +1,15 @@
+function main()
+{
+    let iNo1=10, iNo2= 11;
+
+    if(iNo1> iNo2)
+    {
+        console.log("iNo1 is max");
+    }
+    else
+    {
+        console.log("iNo2 is max");
+    }
+}
+
+main();
