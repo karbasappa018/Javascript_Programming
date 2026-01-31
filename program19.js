a56263a8198d40ab7914f29c9e48be40755b23fe
karbasappa018 <karbasappatadakale@gmail.com>
2026-01-31 22:49:51 +0530

    Add function to check whether a number is even or odd

---
+++ program19.js
@@ -0,0 +1,21 @@
+function checkEvenOdd(no)
+{
+    if(no%2 == 0)
+    {
+        console.log("Number is even");
+
+    }
+    else
+    {
+        console.log("Number is odd");
+    }
+
+}
+function main()
+{
+    let iValue = 11;
+
+    checkEvenOdd(iValue);
+}
+
+main();
