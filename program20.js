de77392b5d20992171e3f9d5c079e1adf22a808b
karbasappa018 <karbasappatadakale@gmail.com>
2026-01-31 22:51:08 +0530

    Add factorial function using iterative approach

---
+++ program20.js
@@ -0,0 +1,22 @@
+function Factorial(no)
+{
+    let iCnt = 0;
+    let iFact = 1;
+
+    for(iCnt = 1;iCnt<=no; iCnt++)
+    {
+        iFact = iFact *iCnt;
+    }
+
+    return iFact;
+}
+
+function main()
+{
+    let iValue = 5;
+   
+    let iRet = Factorial(iValue);
+    console.log("Factorial of number is:"+iRet+"\n");
+}
+
+main();
