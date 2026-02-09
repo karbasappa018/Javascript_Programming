ca5b78c0bf0b5e11a85275e40d9b05bb7c7dbf16
karbasappa018 <karbasappatadakale@gmail.com>
2026-02-09 22:56:06 +0530

    String in javascript

---
+++ String.js
@@ -0,0 +1,18 @@
+function main()
+{
+    let str = "Hello";
+    console.log(typeof(str));  // String
+    
+    let strx = 'Hello';
+    console.log(typeof(strx));  // String
+
+    // use of charAt()
+
+    console.log(str.charAt(0));
+    console.log(str.charAt(1));
+    console.log(str.charAt(2));
+    console.log(str.charAt(3));
+    console.log(str.charAt(4));
+}
+
+main();
